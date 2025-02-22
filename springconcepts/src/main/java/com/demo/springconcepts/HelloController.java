6fea217e813deacdc0834637c2a4a01957c6aeaf
redbyte09 <sakshisharma4234@gmail.com>
2025-02-22 10:17:37 +0530

    Update HelloController.java

--- springconcepts/src/main/java/com/demo/springconcepts/HelloController.java
+++ springconcepts/src/main/java/com/demo/springconcepts/HelloController.java
@@ -10,19 +10,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    //Correct SLF4J Logger
+    // SLF4J Logger
     private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-    //UC1: Simple Hello World
+    private final EmployeeBean employee;
+
+
+    public HelloController(EmployeeBean employee) {
+        this.employee = employee;
+    }
+
+    // UC1: Simple Hello World
     @GetMapping("/uc1")
     public String sayHelloUC1() {
         return "Hello from BridgeLabz";
     }
 
-    //UC2: Hello World with Logging
+    // UC2: Hello World with Logging
     @GetMapping("/uc2")
     public String sayHelloUC2() {
         logger.info("GET request received at /hello/uc2");
         return "Hello from BridgeLabz with Logging";
-}
+    }
+
+    // UC3: Fetch Employee Details (Dependency Injection)
+    @GetMapping("/details")
+    public String getEmployeeDetailsUC3() {
+        return employee.getEmployeeDetails();
+    }
 }
